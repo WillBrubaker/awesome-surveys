09ff7d890b82caf1f557d57f61ea27a757c51467
Will <will@willthewebmechanic.com>
2015-01-16 15:14:54 -0600

    more of the same

--- options.php
+++ options.php
@@ -7,6 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( isset( $_POST ) && ! empty( $_POST ) ) {
 	error_log( print_r( $_POST, true ) );
 	if ( ! current_user_can( 'manage_options' ) || ! wp_verify_nonce( $_POST['_nonce'], 'awesome-surveys-update-options' ) ) {
-	wp_die( __( 'Cheatin&#8217; uh?' ), 403 );
-}
+		wp_die( __( 'Cheatin&#8217; uh?' ), 403 );
+	}
 }
