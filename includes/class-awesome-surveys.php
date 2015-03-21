c32e970554bb541a476738eef29ae51d8c56a7d4
Will <will@willthewebmechanic.com>
2015-03-21 18:13:29 -0500

    Revert "filter the submit button text"
    This reverts commit 108ebbc2ab28e9ba758abda85d230da000f0a719.

--- includes/class-awesome-surveys.php
+++ includes/class-awesome-surveys.php
@@ -314,7 +314,7 @@ class Awesome_Surveys {
 		$form_output->addElement( new Element_Hidden( 'answer_survey_nonce', $nonce ) );
 		$form_output->addElement( new Element_Hidden( 'survey_id', '', array( 'value' => $args['survey_id'], ) ) );
 		$form_output->addElement( new Element_Hidden( 'action', 'answer-survey' ) );
-		$form_output->addElement( new Element_Button( apply_filters( 'wwm_as_submit_button_text', __( 'Submit Response', 'awesome-surveys' ) ), 'submit', array( 'class' => 'button-primary', 'disabled' => 'disabled' ) ) );
+		$form_output->addElement( new Element_Button( __( 'Submit Response', 'awesome-surveys' ), 'submit', array( 'class' => 'button-primary', 'disabled' => 'disabled' ) ) );
 		return $form_output->render( true );
 	}
 
