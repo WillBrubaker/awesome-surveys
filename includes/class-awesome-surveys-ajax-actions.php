9b448a078a92639212c74e87ad01ad31961e87b0
Will <will@willthewebmechanic.com>
2015-12-03 18:55:48 -0600

    oh..so that's how that's done

--- includes/class-awesome-surveys-ajax-actions.php
+++ includes/class-awesome-surveys-ajax-actions.php
@@ -374,9 +374,9 @@ class Awesome_Surveys_Ajax extends Awesome_Surveys {
 		if ( $this->is_captcha_enabled_for_post( $survey_id ) ) {
 			$args = array( 'body' => array( 'secret' => $options['general_options']['captcha_secret_key'], 'response' => $_POST['g-recaptcha-response'] ) );
 			$recaptcha_response = wp_remote_post( 'https://www.google.com/recaptcha/api/siteverify', $args );
-			$recaptcha_success = json_decode( $recaptcha_response['body'], true );
-			if ( ! $recaptcha_success['success'] ) {
-				wp_send_json_error( array( 'error' => $recaptcha_success['error-codes'][0], ) );
+			$recaptcha_success = json_decode( $recaptcha_response['body'] );
+			if ( ! $recaptcha_success->success ) {
+				wp_send_json_error( array( 'error' => $recaptcha_success->{'error-codes'}[0] ) );
 			}
 		}
 		$post = get_post( $survey_id, 'OBJECT', 'display' );
